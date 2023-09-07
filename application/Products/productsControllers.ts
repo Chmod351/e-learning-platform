065b916c10c7daf1862cb8eca1d043bcb389c3cd
yamil351 <leokasper@live.com.ar>
2023-09-07 20:02:56 -0300

    Fix: id values from update and delete operations

--- application/Products/productsControllers.ts
+++ application/Products/productsControllers.ts
@@ -5,29 +5,37 @@ class ProductController {
         const products = await productService.findAll()
         res.status(200).json(products)
     }
+    
     async findByQuery(req: any, res: any) {
         const query: string = req.params.query
         const products = await productService.findByQuery(query)
         res.status(200).json(products)
     }
+
     async findById(req: any, res: any) {
         const id: string = req.params._id
         const product = await productService.findById(id)
         res.status(200).json(product)
     }
-    async create(req: any, res: any) {
+
+    async create(req:any, res:any) {
+        console.log(req.body)
         const { name, description, image_url, price } = req.body
         const createdProduct = await productService.createProduct({ name, description, image_url, price })
         res.status(200).json(createdProduct)
     }
+
     async update(req: any, res: any) {
-        const id: string = req.params.id
+        const id: string = req.params._id
+        console.log(req.params.id)
         const { name, description, image_url, price } = req.body
         const updatedProduct = await productService.updateProduct(id, { name, description, image_url, price });
         res.status(200).json(updatedProduct)
     }
+
     async delete(req: any, res: any) {
-        const id: string = req.params.id
+        console.log(req.params.id)
+        const id: string = req.params._id
         const deletedProduct = await productService.deleteProduct(id)
         res.status(200).json(deletedProduct)
     }
--- application/Products/productsServices.ts
+++ application/Products/productsServices.ts
@@ -24,6 +24,7 @@ class ProductServices {
         }
     }
     async createProduct(body: object) {
+        console.log(body)
         try {
             return await productRepository.create(body)
         } catch (error: any) {
