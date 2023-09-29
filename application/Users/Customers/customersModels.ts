0894f23443889e3a8f7915d03e43c2d03185cb27
yamil351 <leokasper@live.com.ar>
2023-09-29 15:16:48 -0300

    Feature:Customer model

---
+++ application/Users/Customers/customersModels.ts
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+export interface ICart {
+  cartId: mongoose.Types.ObjectId;
+}
+
+export interface IProduct {
+  productId: mongoose.Types.ObjectId;
+}
+
+export interface ICustomer {
+  username: string;
+  password: string;
+  email: string;
+  cartId: ICart;
+  wishList: IProduct[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const CustomerSchema = new mongoose.Schema<ICustomer>(
+  {
+    username: {
+      type: String,
+      required: [true, 'The username is required'],
+      maxLength: [50, 'The max length for the username is 50 characters'],
+      minlength: [3, 'The username must have at least 3 characters'],
+    },
+    password: {
+      type: String,
+      required: [true, 'The password is required'],
+      minlength: [8, 'The password must have at least 8 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'The email is required'],
+      maxlength: [320, 'Email address is too long'],
+      validate: {
+        validator: function (value: string) {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(value);
+        },
+        message: 'Invalid email format',
+      },
+    },
+    cartId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Cart',
+    },
+    wishList: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const Customer = mongoose.model<ICustomer>('Customer', CustomerSchema);
+export default Customer;
