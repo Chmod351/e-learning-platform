9dbbd7ada7d07159bd590fb3dabf66b371a79bc2
yamil351 <leokasper@live.com.ar>
2023-10-01 22:57:56 -0300

    HF:architecture fix

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
 import express, { Application } from 'express';
-import server from './config/dbConfig';
-import products from './application/Products/productsRoutes';
-import customer from './application/Users/Customers/customersRoutes'
-import middlewares from './middlewares/middlewares';
-import errorHandler from './helpers/errorHandler';
+import server from './src/config/dbConfig';
+import products from './src/application/Products/productsRoutes';
+import customer from './src/application/Users/Customers/customersRoutes';
+import middlewares from './src/middlewares/middlewares';
+import errorHandler from './src/helpers/errorHandler';
 
 // config
 const PORT: number = 4000;
@@ -14,7 +14,7 @@ middlewares(app);
 
 // endpoints
 app.use('/api/v1/products', products);
-app.use('/api/v1/customer',customer)
+app.use('/api/v1/customer', customer);
 
 app.use(errorHandler);
 
